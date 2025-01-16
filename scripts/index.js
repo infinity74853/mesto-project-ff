43d80f069fd1a89f59437aed03341d204139a52e
infinity <igor74853@yandex.ru>
2025-01-17 00:44:21 +0500

    17.01.2025 00/44

--- scripts/index.js
+++ scripts/index.js
@@ -9,3 +9,16 @@
 
 // @todo: Вывести карточки на страницу
 
+
+const list = document.querySelector('.card-template');
+
+const listElements = [];
+for (let i = 0; i < list.length; i++) {
+  const listItem = document.createElement('li');
+  listItem.textContent = list[i];
+    listElements[i] = listItem;
+}
+
+for (let i = 0; i < taskElements.length; i++) {
+    list.append(listElements[i])
+} 
