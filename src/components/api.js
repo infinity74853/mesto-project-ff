16bfe100bdb13218869e071d8ef01d6bdf06d46c
infinity <igor74853@yandex.ru>
2025-03-07 06:40:20 +0500

    07.03.2025 06/34

--- src/components/api.js
+++ src/components/api.js
@@ -1,18 +1,13 @@
-  //информация пользователя
-export function apiUserInfo ({profileName, profileDescription, profileImage}) {
-  const renderProfileinfo = (data) => {
-    profileName.textContent = data.name;
-    profileDescription.textContent = data.about;
-    profileImage.src = data.avatar;
-  }
-  
-  return fetch('https://nomoreparties.co/v1/wff-cohort-33/cards', {
+// запрос к серверу 
+export const getUserInfo = () => { 
+  return fetch('https://nomoreparties.co/v1/wff-cohort-33/users/me', {
     headers: {
-      authorization: '477347bb-d8bc-4dbf-8478-d3e39b227db4'
-    }
+      authorization: '477347bb-d8bc-4dbf-8478-d3e39b227db4',
+      'Content-Type': 'application/json',
+    },
   })
-    .then(res => res.json())
-    .then(data => {
-      
-    })
+  .then(res => res.json())
+  .then((result) => {
+    console.log(result);
+  }); 
 }
